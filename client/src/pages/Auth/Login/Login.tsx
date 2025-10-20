7b2507709617e95cd5fe6ffe3b7bc9a402e9ce58
Aryan Maurya <thesoftaryan@gmail.com>
2025-10-20 17:01:52 +0530

    Putting banner image in login page and making it responsive

--- client/src/pages/Auth/Login/Login.tsx
+++ client/src/pages/Auth/Login/Login.tsx
@@ -9,32 +9,45 @@ import GapBox from "../../../components/common/GapBox/GapBox";
 import GoogleSocialSignInIcon from "../../../assets/icons/google.svg?react";
 import FacebookSocialSignInIcon from "../../../assets/icons/facebook.svg?react";
 
+// Images
+import BannerImageLight from "../../../assets/images/Authentication/banner-light.png";
+// import BannerImageDark from "../../../assets/images/Authentication/banner-dark.png";
+
 // Style
 import LoginStyle from "./Login.module.css";
 
-function Login(){
+
+
+function Login() {
     return <>
         <div className={LoginStyle["container"]}>
-
-            <div className={LoginStyle["header"]}>
-                <h1 className={LoginStyle["heading"]}> Login to Account</h1>
-                <p className={LoginStyle["signup"]}>Don't have an account? <a href="#">SignUp</a></p>
+            <div className={LoginStyle["left-container"]}>
+                <div className={LoginStyle["image-wrapper"]}>
+                    <img src={BannerImageLight} alt="Banner-Image" className={LoginStyle["banner-image"]}/>
+                </div>
             </div>
-            
-            
 
-            <div className={LoginStyle["login-form"]}>
-                <InputField placeholder="Email" type="email"/>
-                <InputField placeholder="Password" type="password"/>
-                <CheckBox text={"Remember me"}></CheckBox>
+            <div className={LoginStyle["right-container"]}>
+                <div className={LoginStyle["header"]}>
+                    <h1 className={LoginStyle["heading"]}> Login to Account</h1>
+                    <p className={LoginStyle["signup"]}>Don't have an account? <a href="#">SignUp</a></p>
+                </div>
 
-                {/* Gaps are already defined in index.css inside theme directory */}
-                <GapBox className={"gap-y-medium"}/>
 
-                <Button text="Login"></Button>
-                <Divider text="Or login with"></Divider>
-                <SocialSignInCard text="Google" Icon={GoogleSocialSignInIcon} onClick={()=>{console.log("Social sign in button clicked.")}}/>
-                <SocialSignInCard text="Facebook" Icon={FacebookSocialSignInIcon} onClick={()=>{console.log("Social sign in button clicked.")}}/>
+
+                <div className={LoginStyle["login-form"]}>
+                    <InputField placeholder="Email" type="email" />
+                    <InputField placeholder="Password" type="password" />
+                    <CheckBox text={"Remember me"}></CheckBox>
+
+                    {/* Gaps are already defined in index.css inside theme directory */}
+                    <GapBox className={"gap-y-medium"} />
+
+                    <Button text="Login"></Button>
+                    <Divider text="Or login with"></Divider>
+                    <SocialSignInCard text="Google" Icon={GoogleSocialSignInIcon} onClick={() => { console.log("Social sign in button clicked.") }} />
+                    <SocialSignInCard text="Facebook" Icon={FacebookSocialSignInIcon} onClick={() => { console.log("Social sign in button clicked.") }} />
+                </div>
             </div>
         </div>
     </>;
