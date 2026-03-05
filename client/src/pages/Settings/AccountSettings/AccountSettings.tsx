d1ea72c0cb2706b20f6611c6ce232ee5fb7a8f9a
Aryan Maurya <thesoftaryan@gmail.com>
2026-03-05 14:02:21 +0530

    Working on AccountSettings page

--- client/src/pages/Settings/AccountSettings/AccountSettings.tsx
+++ client/src/pages/Settings/AccountSettings/AccountSettings.tsx
@@ -5,50 +5,91 @@ import ChangeIcon from "../../../assets/icons/general/change.svg?react";
 import SaveIcon from "../../../assets/icons/general/save.svg?react";
 import DeleteIcon from "../../../assets/icons/general/delete.svg?react";
 import passwordIcon from "../../../assets/icons/general/password.svg?react";
+import InstagramIcon from "../../../assets/icons/profile/instagram.svg?react";
+import FacebookIcon from "../../../assets/icons/profile/facebook.svg?react";
+import LinkedinIcon from "../../../assets/icons/profile/linkedin.svg?react";
+import YoutubeIcon from "../../../assets/icons/profile/youtube.svg?react";
+
 import Divider from "../../../components/common/Divider/Divider";
 import { color } from "../../../theme/ThemeVariables";
 import { UserProfile } from "../../../components/common/UserProfile/UserProfile";
 import InputField from "../../../components/common/InputField/InputField";
 import { useAppSelector } from "../../../hooks/store.hook";
 import TextEditor from "../../../components/common/TextEditor/TextEditor";
+import { TagChip } from "../../../components/common/TagChip/TagChip";
+import Button from "../../../components/common/Button/Button";
 
 export const AccountSettings = ()=>{
     const user = useAppSelector(state=>state.auth.user);
     return (
-        <div className="container">
-            <div className="profile-details">
-                <Divider text="Set your profile details"/>
-                <div className="user-profile-settings">
-                    <UserProfile/>
-                    <div className="user-profile-actions">
-                        <DeleteIcon/>
-                        <ChangeIcon/>
+        <div className={AccountSettingsStyle["container"]}>
+            <Divider text="Set your profile details"/>
+            <div className={AccountSettingsStyle["profile-details"]}>
+                <div className={AccountSettingsStyle["user-profile-settings"]}>
+                    <UserProfile className={AccountSettingsStyle["user-profile"]}/>
+                    <div className={AccountSettingsStyle["user-profile-actions"]}>
+                        <DeleteIcon className={AccountSettingsStyle["icon"]}/>
+                        <ChangeIcon className={AccountSettingsStyle["icon"]}/>
                     </div>
                 </div>
-                <div className="user-details-settings">
-                    <div className="user-name">
+                <div className={AccountSettingsStyle["user-details-settings"]}>
+                    <div className={AccountSettingsStyle["user-name"]}>
                         <InputField placeholder="" value={user?.first_name}/>
                         <InputField placeholder="" value={user?.last_name}/>
                     </div>
                     <InputField placeholder="" value={user?.email} disabled={true}/>
-                    <div className="user-education">
+                    <div className={AccountSettingsStyle["user-education"]}>
                         <InputField placeholder="" value="B.Tech"/>
                         <InputField placeholder="" value="IIT Bhilai"/>
                     </div>
                     
                     <TextEditor onChange={()=>{}}/>
 
-                    <SettingsActionButton text="Save Details" Icon={SaveIcon}/>
+                    <div className={AccountSettingsStyle["center-button"]}>
+                        <SettingsActionButton text="Save Details" Icon={SaveIcon}/>
+                    </div>
                 </div>
             </div>
-            <div className="social-links">
-                <Divider text="Set your social links"/>
+            <Divider text="Set your social links"/>
+            <div className={AccountSettingsStyle["social-links"]}>
+                <div className={AccountSettingsStyle["social-links-group"]}>
+                    <InstagramIcon className={AccountSettingsStyle["icon"]}/>
+                    <InputField placeholder="Instagram Profile Link"/>
+                </div>
+                <div className={AccountSettingsStyle["social-links-group"]}>
+                    <FacebookIcon className={AccountSettingsStyle["icon"]}/>
+                    <InputField placeholder="Facebook Profile Link"/>
+                </div>
+                <div className={AccountSettingsStyle["social-links-group"]}>
+                    <LinkedinIcon className={AccountSettingsStyle["icon"]}/>
+                    <InputField placeholder="Linkedin Profile Link"/>
+                </div>
+                <div className={AccountSettingsStyle["social-links-group"]}>
+                    <YoutubeIcon className={AccountSettingsStyle["icon"]}/>
+                    <InputField placeholder="Youtube Channel Link"/>
+                </div>
             </div>
-            <div className="knowledge-tags">
-                <Divider text="Set your knowledge tags"/>
+            <Divider text="Set your knowledge tags"/>
+            <div className={AccountSettingsStyle["knowledge-tags"]}>
+                {/* <div className={AccountSettingsStyle["tag-input-label"]}>Add Tags</div>
+                <div className={AccountSettingsStyle["tag-input"]}>
+                    <InputField placeholder="Add new knowledge tag"/>
+                </div> */}
+                <div className={AccountSettingsStyle["knowledge-tag-input-container"]}>
+                    <input type="text" placeholder="Enter your tag" className={AccountSettingsStyle["knowledge-tag-input"]}/>
+                    <div className={AccountSettingsStyle["add-knowledge-tag"]}>
+                        <Button text="Add Tag" level2={true} isSmall={true}/>
+                    </div>
+                </div>
+                <div className={AccountSettingsStyle["tags-container"]}>
+                    <TagChip text="Neuro" color="purple" level1={true}/>
+                    <TagChip text="Neuro" color="purple" level1={true}/>
+                    <TagChip text="Neuro" color="purple" level1={true}/>
+                    <TagChip text="Neuro" color="purple" level1={true}/>
+                </div>
             </div>
-            <div className="danger-zone">
-                <Divider text="Danger Zone" color={color.colorDanger}/>
+            <Divider text="Danger Zone" color={color.colorDanger}/>
+            <div className={AccountSettingsStyle["danger-zone"]}>
                 <SettingsActionButton text="Delete Account" Icon={DeleteIcon} color={color.colorDanger}/>
                 <SettingsActionButton text="Change Password" Icon={passwordIcon} color={color.colorOrange}/>
             </div>
