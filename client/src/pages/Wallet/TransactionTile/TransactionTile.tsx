4c1b32dea72dd1a87acaf7c07d5145c009417ddb
Aryan Maurya <thesoftaryan@gmail.com>
2026-06-17 17:00:37 +0530

    Working on payment system

--- client/src/pages/Wallet/TransactionTile/TransactionTile.tsx
+++ client/src/pages/Wallet/TransactionTile/TransactionTile.tsx
@@ -3,9 +3,10 @@ import TransactionTileStyle from "./TransactionTile.module.css";
 
 interface TransactionTileProps{
     received: boolean,
+    money?: number,
 }
 
-export const TransactionTile:React.FC<TransactionTileProps> = ({received})=>{
+export const TransactionTile:React.FC<TransactionTileProps> = ({received, money=200})=>{
     return (
         <div className={TransactionTileStyle["container"]}>
             <div className={TransactionTileStyle["time"]}>{formatDate(new Date(Date.now()))}</div>
@@ -19,7 +20,7 @@ export const TransactionTile:React.FC<TransactionTileProps> = ({received})=>{
             <div className={`${TransactionTileStyle["amount"]} ${received? TransactionTileStyle["received"]:""}`}>
                 {
                     received? "+":"-"
-                }120
+                }{money}
             </div>
         </div>
     )
--- client/src/pages/Wallet/Wallet.tsx
+++ client/src/pages/Wallet/Wallet.tsx
@@ -5,20 +5,32 @@ import WalletStyle from "./Wallet.module.css";
 export const Wallet = ()=>{
     return (
         <div className={WalletStyle["wallet-container"]}>
-            <div className="wallet-stats">
-                <div className="wallet-balance"></div>
-                <div className="wallet-transaction-count">
-                    <div className="count-label"></div>
-                    <div className="amount"></div>
-                    <Divider/>
-                    <div className="count-label"></div>
-                    <div className="amount"></div>
+            <div className={WalletStyle["wallet-stats"]}>
+                <div className={WalletStyle["wallet-balance"]}>
+                    <div className={WalletStyle["heading"]}>
+                        PulsePoints
+                    </div>
+                    <div className={WalletStyle["balance"]}>
+                        4728
+                    </div>
+                </div>
+                <div className={WalletStyle["vertical-divider"]}></div>
+                <div className={WalletStyle["wallet-transaction-amount"]}>
+                    <div className={WalletStyle["amount-label"]}>Total points sent</div>
+                    <div className={WalletStyle["amount"]}>2819</div>
+                    <div className={WalletStyle["divider"]}>
+                        <Divider/>
+                    </div>
+                    <div className={WalletStyle["amount-label"]}>Total points received</div>
+                    <div className={WalletStyle["amount"]}>7547</div>
                     
                 </div>
             </div>
-            <Divider text="Transaction History"/>
-            <div className="transactions-container">
-                <TransactionTile received={true}/>
+            <div className={`${WalletStyle["divider"]} ${WalletStyle["margin-top"]}`}>
+                <Divider text="Transaction History"/>
+            </div>
+            <div className={WalletStyle["transactions-container"]}>
+                <TransactionTile received={true} money={5000}/>
                 <TransactionTile received={false}/>
             </div>
         </div>
